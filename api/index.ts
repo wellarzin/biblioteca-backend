439b2982d75dbdf6b2e9edd1a5aa15eb629db6f3
wellarzin <brunodiaswwll@gmail.com>
2025-12-08 22:10:04 -0300

    asdas

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
+import app from "../dist/index";
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "./index";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
+  app(req, res);
 }
