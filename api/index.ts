d683dc2e72a3d4f972aeb66d881b19c8bb53ca20
wellarzin <brunodiaswwll@gmail.com>
2025-12-08 22:17:33 -0300

    alpsdlasd

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import serverlessExpress from "@vendia/serverless-express";
-import app from "../dist/index";
+import app from "../src/index"; // ← pega o express antes do build
 
 const handler = serverlessExpress({ app });
 
